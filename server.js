797af221842e7f44e89dc7cd5599b5bc6908a076
Pah <pah.webdev@gmail.com>
2018-08-09 18:42:10 -0300

    creating file to conect with db and start app

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+//Importing Files to handle requests of specific route
+const users = require("./routes/users");
+const pets = require("./routes/pets");
+
+//Creating a new Express Application
+const app = express();
+//Choosing the port
+const port = process.env.PORT || 3000;
+
+//Connecting to database
+const db = require("./configs/keys").mongoURI;
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("Database Connected."))
+  .catch(err => console.log(err));
+
+//Adding Body Parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//Routes
+app.use("/users", users);
+app.use("/pets", pets);
+
+app.listen(port, () => console.log(`Server in port: ${port}`));
