f4c4ead7f27c866e5899aa33ac9e58f6e70fc8e2
Pah <pah.dsf@gmail.com>
2018-09-19 18:43:49 -0300

    Rota para criação de PET

--- routes/api/pets.js
+++ routes/api/pets.js
@@ -1,13 +1,35 @@
 const express = require("express");
 const router = express.Router();
+const passport = require("passport");
+
+const Pet = require("../../models/Pet");
 
 /*
-  Route: GET to pets/test
+  Route: GET to api/pets/test
   Description: Test Route to Pets
   Access: public
 */
 router.get("/test", (req, res) => {
   res.json({ message: "Successful Test to pets." });
 });
+
+/*
+  Route: POST to api/pets/create
+  Description: Rota para criar pet
+  Access: private
+*/
+router.post('/create', passport.authenticate('jwt', { session: false }), (req, res) => {
+  const newPet = new Pet({
+    nome: req.body.nome,
+    sexo: req.body.sexo,
+    descricao: req.body.descricao,
+    porte: req.body.porte,
+    fotos: req.body.fotos,
+    responsavel: req.user.id
+  });
+  newPet.save()
+    .then(pet => res.json(pet))
+    .catch(err => console.log(err));
+})
 
 module.exports = router;
