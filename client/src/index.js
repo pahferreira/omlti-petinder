87a004efe76d3a7a389b4d9531d380ca84918711
Aaron Santos <aaronpedro16@gmail.com>
2018-10-29 11:21:03 -0300

    Criando Mural de pets

--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import PetMural from "./components/PetMural/PetMural"
+
+
+const Teste = <div>
+    <PetMural />
+</div> 
+
+ReactDOM.render( Teste, document.getElementById("root"))
