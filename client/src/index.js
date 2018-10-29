cec396d6122db39c715b25f1c2e35b01279d5e86
joaopab1337 <joao.pedro@academico.ifpb.edu.br>
2018-10-29 14:00:03 -0300

    Adicionando index e alterando instruções no readme

--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./index.css"
 
+import NavBar from "./components/NavBar/NavBar";
+import LoginForm from "./components/LoginForm/LoginForm";
+
+const App = () => (
+	<div>
+		<NavBar />
+		<LoginForm />
+	</div>
+	
+);
+
+ReactDOM.render(<App />, document.getElementById("root"));
