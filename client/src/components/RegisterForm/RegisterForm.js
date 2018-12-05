6342dc80436f4d485373d332c695ac629a5d4cc5
João Pedro <joaopab1337@gmail.com>
2018-12-04 22:22:19 -0300

    Adicionando axios e realizando alterações referentes ao processo de cadastro de novos usuarios

--- client/src/components/RegisterForm/RegisterForm.js
+++ client/src/components/RegisterForm/RegisterForm.js
@@ -1,12 +1,28 @@
 import React, { Component } from "react";
 import { Grid, Button, TextField, Typography } from '@material-ui/core';
 import "./RegisterForm.css";
+import axios from "axios";
 
 class RegisterForm extends Component {
+	
+	onSubmit(e) {
+		e.preventDefault();
+		const form = e.target;
+		const newUser = {};
+		for(let i=0; i<form.length; i++) {
+			if(form[i].localName === "input"){
+				let input = form[i];
+				newUser[input.name] = input.value;
+			}
+		}
+		axios.post("/api/users/register", newUser);
+		document.location.href = "/";
+	}
+
 
 	render() {
 		return (
-			<form className="register-container">
+			<form onSubmit={this.onSubmit}  className="register-container">
 				<Grid container justify="center" alignItems="center">
 					<div>
 						<Typography variant="title" align="center">
@@ -17,12 +33,14 @@ class RegisterForm extends Component {
 							style={style}
 							placeholder="Digite seu nome completo"
 							label="Nome"
+							name="nome"
 						/>
 						<br />
 						<TextField
 							style={style}
 							placeholder="Digite seu e-mail"
 							label="E-mail"
+							name="email"
 						/>
 						<br />
 						<TextField
@@ -30,9 +48,10 @@ class RegisterForm extends Component {
 							type="password"
 							placeholder="Digite sua senha"
 							label="Senha"
+							name="senha"
 						/>
 						<br />
-						<Button variant="raised" size="medium" primary={true} style={style} onClick={(event) => this.handleClick(event)}>Cadastrar</Button>
+						<Button variant="contained" size="medium"  style={style} type="submit">Cadastrar</Button> 
 						<br />
 						<Typography align="center">
 							<a href="/">
